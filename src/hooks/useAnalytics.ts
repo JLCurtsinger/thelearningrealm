2a4e2e3482949a1bde49219c50777492a5c79da2
Justin <98992595+JLCurtsinger@users.noreply.github.com>
2025-05-17 16:43:58 -0700

    Create useAnalytics.ts

---
+++ src/hooks/useAnalytics.ts
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+
+export const useAnalytics = () => {
+  useEffect(() => {
+    const GA_MEASUREMENT_ID = 'G-MWWWEBNPLXS';
+
+    // Inject the GA script into the <head>
+    const script = document.createElement('script');
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+    script.async = true;
+    document.head.appendChild(script);
+
+    // Set up gtag function and dataLayer
+    window.dataLayer = window.dataLayer || [];
+    function gtag(...args: any[]) {
+      window.dataLayer.push(args);
+    }
+    window.gtag = gtag;
+
+    // Initialize GA
+    gtag('js', new Date());
+    gtag('config', GA_MEASUREMENT_ID);
+  }, []);
+};
